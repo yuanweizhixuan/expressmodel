212b58f250f00388e9aa66efb1d3d536e163e492
QIXIUQX <qingwu13950520@outlook.com>
2023-05-10 16:15:59 +0800

    测试签到

--- src/app.js
+++ src/app.js
@@ -1,9 +1,3 @@
-/*
- * @Author: QIXIU
- * @Description: Do not edit
- * @Date: 2023-05-10 16:10:40
- * @LastEditTime: 2023-05-10 16:11:15
- */
 const express = require("express");
 var bodyParser = require("body-parser");
 
@@ -18,5 +12,16 @@ app.all("*", function (req, res, next) {
 	res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
 	next();
 });
+
+// 签到 增加新的签到功能测试
+app.post("/signin", function (req, res) {
+	res.send({
+		// begin_time: "Oct 22, 2021 2:56:01 PM",
+		begin_time: "Oct 22, 2021 3:55:01 PM",
+		status: 0,
+		current_time: "Oct 22, 2021 3:56:01 PM",
+	});
+});
+
 
 app.listen(3000, () => console.log("express 正在运行， 端口3000"));
